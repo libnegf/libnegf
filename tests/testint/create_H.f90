3d1a21f2f886b352f249a6746ce2681a5ff3767f
mgaleotti <mgaleotti@6ef4337c-0ab6-4091-bd9f-132bc75f0ffb>
2010-10-07 14:49:36 +0000

    modified test for A-B chain

--- tests/testint/create_H.f90
+++ tests/testint/create_H.f90
@@ -6,21 +6,30 @@ program create_H
   open(101,file='H_real.dat')
   open(102,file='H_imm.dat')  
 
-  write(101,*) "% Size = 10 10" 
-  write(101,*) "% Nonzeros = 18" 
-  write(101,*) "zzz = zeros(18,3);"
+  write(101,*) "% Size = 100 100" 
+  write(101,*) "% Nonzeros = 298" 
+  write(101,*) "zzz = zeros(298,3);"
   write(101,*) "zzz = ["
 
-  write(102,*) "% Size = 10 10" 
-  write(102,*) "% Nonzeros = 18" 
-  write(102,*) "zzz = zeros(18,3);"
+  write(102,*) "% Size = 100 100" 
+  write(102,*) "% Nonzeros = 298" 
+  write(102,*) "zzz = zeros(298,3);"
   write(102,*) "zzz = ["
 
-  do i=1,9
+  do i=1,99,2
+     write(101,*) i,i,-2.0d0
+     write(101,*) i+1,i+1,2.0d0
      write(101,*) i,i+1,1.d0
      write(101,*) i+1,i,1.d0
+     write(101,*) i+1,i+2,1.d0
+     write(101,*) i+2,i+1,1.d0
+ 
+     write(102,*) i,i,0.d0
+     write(102,*) i+1,i+1,0.d0
      write(102,*) i,i+1,0.d0
      write(102,*) i+1,i,0.d0
+     write(102,*) i+1,i+2,0.d0
+     write(102,*) i+2,i+1,0.d0
   end do
 
   write(101,*) "]"
