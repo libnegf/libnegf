782e577ec8fdf0d17661104c037a36f0bd3865f8
pecchia <pecchia@6ef4337c-0ab6-4091-bd9f-132bc75f0ffb>
2010-12-23 13:11:50 +0000

    added set_defaults() and other sets in lib_param

--- src/lib_param.F90
+++ src/lib_param.F90
@@ -22,25 +22,26 @@ module lib_param
      character(LST) :: file_re_S
      character(LST) :: file_im_S
      character(LST) :: file_struct
-     character(1) :: DorE           !Density or En.Density
-     type(format) :: form
+     character(1) :: DorE              ! Density or En.Density
+     type(format) :: form              ! Form of file-Hamiltonian
 
-     logical   :: ReadoldSGF
-     logical   :: FictCont(MAXNCONT)
+     logical   :: ReadoldSGF           ! Read computed Surface G.F.
+     logical   :: FictCont(MAXNCONT)   ! Ficticious contact 
 
-     real(dp) :: mu(MAXNCONT)    ! Potenziale elettrico
-     real(dp) :: Efermi(MAXNCONT)! Energia di Fermi dei contatti
-     real(dp) :: contact_DOS(MAXNCONT)
+     real(dp) :: mu(MAXNCONT)          ! Potenziale elettrico
+     real(dp) :: Efermi(MAXNCONT)      ! Energia di Fermi dei contatti
+     real(dp) :: contact_DOS(MAXNCONT) ! Ficticious contact DOS
 
      real(dp) :: mu_n
      real(dp) :: mu_p
      real(dp) :: Ec
      real(dp) :: Ev
      real(dp) :: DeltaEc
-     real(dp) :: DeltaEv     
-     real(dp) :: E
-     real(dp) :: dos
-     real(dp) :: eneconv
+     real(dp) :: DeltaEv
+
+     real(dp) :: E                 ! Holding variable 
+     real(dp) :: dos               ! Holding variable
+     real(dp) :: eneconv           ! Energy conversion factor
 
      type(z_CSR) :: H
      type(z_CSR) :: S
@@ -50,52 +51,102 @@ module lib_param
      type(z_DNS) :: SC(MAXNCONT)
      type(z_CSR) :: HMC(MAXNCONT)
      type(z_CSR) :: SMC(MAXNCONT)
-     type(z_CSR) :: Gr
-     type(z_CSR) :: rho
-     type(z_CSR) :: rho_eps
-     logical    :: isSid
+     type(z_CSR) :: Gr             ! Holding output Matrix
+     type(z_CSR) :: rho            ! Holding output Matrix
+     type(z_CSR) :: rho_eps        ! Holding output Matrix
+     logical    :: isSid          
 
-     type(TStruct_Info) :: str
+     type(TStruct_Info) :: str     ! system structure
 
-     real(dp) :: delta
-     real(dp) :: Emin
-     real(dp) :: Emax
-     real(dp) :: Estep
-     real(dp) :: Temp
+     real(dp) :: delta       ! delta for G.F. 
+     real(dp) :: Emin        ! Tunneling or dos interval
+     real(dp) :: Emax        ! 
+     real(dp) :: Estep       ! Tunneling or dos E step
+     real(dp) :: Temp        ! electronic temperature
+     real(dp) :: spin        ! spin degeneracy
 
 
-     integer :: Np_n(2)
-     integer :: Np_p(2)
-     !integer :: Np(4)   
-     integer :: n_kt
-     integer :: n_poles
-     integer :: iteration
-     integer :: activecont 
-     integer :: N_omega         ! Numero di kT per l'integrazione
-     real(dp) :: spin   
-     integer :: ni(MAXNCONT)
-     integer :: nf(MAXNCONT)
+     integer :: Np_n(2)      ! Number of points for n 
+     integer :: Np_p(2)      ! Number of points for p 
+     integer :: n_kt         ! Numero di kT per l'integrazione
+     integer :: n_poles      ! Numero di poli 
+     integer :: iteration    ! Iterazione (SCC)
+     integer :: activecont   ! contact selfenergy
+     integer :: ni(MAXNCONT) ! ni
+     integer :: nf(MAXNCONT) ! nf
   
-
   end type Tnegf
 
 contains
   
-  subroutine fill_parameters(negf, verbose, DorE, form, mu_n, mu_p, Ec, Ev, &
-       DeltaEc, DeltaEv, E, dos, eneconv, ReadoldSGF, FictCont, mu, Efermi, &
-       contact_DOS, delta, Emin, Emax, Estep, Temp, Np_n, Np_p, n_kt, n_poles, &
-       iteration, activecont, N_omega, spin)
+  subroutine set_convfactor(negf, eneconv)
+    type(Tnegf) :: negf
+    real(dp) :: eneconv
+    
+    negf%eneconv=eneconv
 
+  end subroutine set_convfactor
+ ! -------------------------------------------------------------------
+  
+  subroutine set_fictcont(negf,cont,dos)
+    type(Tnegf) :: negf
+    integer :: cont
+    real(dp) :: DOS
+ 
+    negf%FictCont(cont) = .true. 
+    negf%contact_DOS(cont) = DOS
+
+  end subroutine set_fictcont
+ ! -------------------------------------------------------------------
+
+  subroutine set_iteration(negf,iter)
+    type(Tnegf) :: negf
+    integer :: iter
+
+    negf%iteration = iter
+  end subroutine set_iteration      
+ ! -------------------------------------------------------------------
+
+  subroutine set_computation(negf,DorE) 
+    type(Tnegf) :: negf
+    character(1) :: DorE           !Density or En.Density
+
+    negf%DorE=DorE
+  end subroutine set_computation      
+ ! -------------------------------------------------------------------
+
+  subroutine set_readOldSGF(negf,logic) 
+    type(Tnegf) :: negf
+    logical :: logic
+
+    negf%ReadoldSGF=logic
+  end subroutine set_readoldsgf    
+ ! -------------------------------------------------------------------
+
+  subroutine set_fermi(negf,ncont,efermi)
+    type(Tnegf) :: negf
+    integer :: ncont
+    real(dp) :: efermi(*)
+    
+    negf%Efermi(1:ncont) = Efermi(1:ncont)
+  end subroutine set_fermi
+
+  subroutine set_potentials(negf,ncont,mu)
+    type(Tnegf) :: negf
+    integer :: ncont
+    real(dp) :: mu(*)
+
+    negf%mu(1:ncont) = mu(1:ncont)
+  end subroutine set_potentials
+ ! -------------------------------------------------------------------
+
+
+  subroutine fill_parameters(negf, verbose, mu_n, mu_p, Ec, Ev, &
+        DeltaEc, DeltaEv, delta, Emin, Emax, Estep, &
+        Temp, Np_n, Np_p, n_kt, n_poles, spin)
 
     type(Tnegf) :: negf
     integer :: verbose
-    character(1) :: DorE           !Density or En.Density
-    type(format) :: form
-    logical   :: ReadoldSGF
-    logical   :: FictCont(MAXNCONT)
-    real(dp) :: mu(MAXNCONT)    ! Potenziale elettrico
-    real(dp) :: Efermi(MAXNCONT)! Energia di Fermi dei contatti
-    real(dp) :: contact_DOS(MAXNCONT)
 
     real(dp) :: mu_n
     real(dp) :: mu_p
@@ -103,9 +154,6 @@ contains
     real(dp) :: Ev
     real(dp) :: DeltaEc
     real(dp) :: DeltaEv     
-    real(dp) :: E
-    real(dp) :: dos
-    real(dp) :: eneconv
 
     real(dp) :: delta
     real(dp) :: Emin
@@ -117,20 +165,9 @@ contains
     integer :: Np_p(2)   
     integer :: n_kt
     integer :: n_poles
-    integer :: iteration
-    integer :: activecont 
-    integer :: N_omega         ! Numero di kT per l'integrazione
     real(dp) :: spin   
 
-
     negf%verbose = verbose
-    negf%DorE = DorE
-    negf%form = form
-    negf%ReadoldSGF = ReadoldSGF
-    negf%FictCont = FictCont
-    negf%mu = mu
-    negf%Efermi = Efermi
-    negf%contact_DOS = contact_DOS
 
     negf%mu_n = mu_n
     negf%mu_p = mu_p
@@ -138,9 +175,6 @@ contains
     negf%Ev = Ev
     negf%DeltaEc = DeltaEc 
     negf%DeltaEv = DeltaEv 
-    negf%E = E
-    negf%dos = dos
-    negf%eneconv = eneconv 
 
     negf%delta = delta
     negf%Emin = Emin
@@ -152,9 +186,6 @@ contains
     negf%Np_p = Np_p
     negf%n_kt = n_kt
     negf%n_poles = n_poles
-    negf%iteration = iteration
-    negf%activecont = activecont
-    negf%N_omega = N_omega
     negf%spin = spin
 
   end subroutine fill_parameters
@@ -176,6 +207,58 @@ contains
 
   end subroutine pass_HS
 
+
+  subroutine set_defaults(negf)
+    type(Tnegf) :: negf    
+
+     negf%verbose = 10
+
+     negf%file_re_H = ''
+     negf%file_im_H = ''
+     negf%file_re_S = ''
+     negf%file_im_S = ''
+     negf%file_struct = ''
+     negf%DorE = 'D'             ! Density or En.Density
+
+     negf%ReadoldSGF = .false.   ! Read computed Surface G.F.
+     negf%FictCont = .false.     ! Ficticious contact 
+
+     negf%mu = 0.d0            ! Potenziale elettrico
+     negf%efermi= 0.d0         ! Energia di Fermi dei contatti
+     negf%contact_DOS = 0.d0   ! Ficticious contact DOS
+
+     negf%mu_n = 0.d0
+     negf%mu_p = 0.d0
+     negf%Ec = 0.d0
+     negf%Ev = 0.d0
+     negf%DeltaEc = 0.d0
+     negf%DeltaEv = 0.d0
+
+     negf%E = 0.d0            ! Holding variable 
+     negf%dos = 0.d0          ! Holding variable
+     negf%eneconv = 0.d0      ! Energy conversion factor
+
+     negf%isSid = .false.         
+
+     negf%delta = 1.d-4      ! delta for G.F. 
+     negf%Emin = 0.d0        ! Tunneling or dos interval
+     negf%Emax = 0.d0        ! 
+     negf%Estep = 0.d0       ! Tunneling or dos E step
+     negf%Temp = 0.d0        ! electronic temperature
+     negf%spin = 2.d0        ! spin degeneracy
+
+     negf%Np_n = (/20, 20/)   ! Number of points for n 
+     negf%Np_p = (/20, 20/)   ! Number of points for p 
+     negf%n_kt = 10          ! Numero di kT per l'integrazione
+     negf%n_poles = 3        ! Numero di poli 
+     negf%iteration = 1      ! Iterazione (SCC)
+     negf%activecont = 0     ! contact selfenergy
+     negf%ni = 0             ! ni
+     negf%ni(1) = 1
+     negf%nf = 0             ! nf
+     negf%nf(1) = 2
+     
+   end subroutine set_defaults
 
 end module lib_param
  
