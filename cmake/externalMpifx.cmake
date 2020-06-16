7aebda5050cc2aee58d1c2e112b74c533548dc5f
Gabriele Penazzi <g.penazzi@gmail.com>
2020-06-16 20:10:20 +0200

    Make the cmake more project-friendly: - Append to modules instead of overwriting - Respect user choice ot MPIFX_LIB - Rename LIBRARY to LIB

--- cmake/externalMpifx.cmake
+++ cmake/externalMpifx.cmake
@@ -3,8 +3,12 @@ find_package(Git REQUIRED)
 
 function(find_or_build_mpifx)
 
+  if (MPIFX_LIB AND MPIFX_INCLUDE_DIR)
+    return()
+  endif()
+
   find_library(
-      MPIFX_LIBRARY
+      MPIFX_LIB
       NAMES mpifx
       PATHS /usr/lib /usr/lib64 /usr/local/lib /usr/local/lib64
       DOC "Find system mpifx")
@@ -13,12 +17,12 @@ function(find_or_build_mpifx)
   get_filename_component(MPIFX_DIRECTORY "${MPIFX_LIBRARY}" DIRECTORY)
   set(MPIFX_INCLUDE_DIR "${MPIFX_DIRECTORY}/../include")
 
-  if (NOT MPIFX_LIBRARY OR NOT EXISTS "${MPIFX_INCLUDE_DIR}/libmpifx_module.mod" OR FORCE_MPIFX_INSTALL)
+  if (NOT MPIFX_LIB OR NOT EXISTS "${MPIFX_INCLUDE_DIR}/libmpifx_module.mod" OR FORCE_MPIFX_INSTALL)
 
     message("mpifx not found: adding a target to install from github")
     set(EXTERNAL_INSTALL_LOCATION ${CMAKE_BINARY_DIR}/external/mpifx)
     set(MPIFX_INCLUDE_DIR ${EXTERNAL_INSTALL_LOCATION}/include PARENT_SCOPE)
-    set(MPIFX_LIBRARY "${EXTERNAL_INSTALL_LOCATION}/lib/libmpifx.a" PARENT_SCOPE)
+    set(MPIFX_LIB "${EXTERNAL_INSTALL_LOCATION}/lib/libmpifx.a" PARENT_SCOPE)
     ExternalProject_Add(ext_mpifx
         GIT_REPOSITORY https://github.com/dftbplus/mpifx/
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_INSTALL_LOCATION}
@@ -29,7 +33,5 @@ function(find_or_build_mpifx)
     )
 
   endif()
-
-#   include_directories(${MPIFX_INCLUDE_DIR})
 
 endfunction()
