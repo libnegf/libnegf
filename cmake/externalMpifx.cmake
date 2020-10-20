e4c969a3d812964288f4fdc2e7e172ee393b7dd1
Bálint Aradi <aradi@uni-bremen.de>
2020-10-20 13:30:29 +0200

    Streamline MpiFx handling
    Remove redundant option of manual library directory specification as
    find_package() with proper CMAKE_PREFIX_PATH does exactly the same.

--- cmake/externalMpifx.cmake
+++ cmake/externalMpifx.cmake
@@ -1,24 +1,12 @@
-function(find_or_build_mpifx)
+function(find_or_fetch_mpifx)
 
   # If MpiFx had been detected already, done.
   if(TARGET MpiFx::MpiFx)
     return()
   endif()
 
-  # If user has set up library explicitely, take that choice withouth further checks.
-  set(MPIFX_LIBRARY "" CACHE STRING "MpiFx library path")
-  set(MPIFX_INCLUDE_DIRECTORY "" CACHE STRING "MpiFx include path")
-  if (MPIFX_LIBRARY AND MPIFX_INCLUDE_DIRECTORY)
-    message(STATUS "Customized MpiFx library: ${MPIFX_LIBRARY}")
-    message(STATUS "Customized MpiFx include directory: ${MPIFX_INCLUDE_DIRECTORY}")
-    add_library(MpiFx::MpiFx INTERFACE IMPORTED GLOBAL)
-    target_link_libraries(MpiFx::MpiFx INTERFACE "${MPIFX_LIBRARY}")
-    target_include_directories(MpiFx::MpiFx INTERFACE "${MPIFX_INCLUDE_DIRECTORY}")
-    return()
-  endif()
-
   if(NOT FORCE_MPIFX_DOWNLOAD)
-    # Find system installed MpiFx package.
+    # Find system installed MpiFx package (provided CMAKE_PREFIX_PATH contains the package root)
     find_package(MpiFx QUIET)
     if(MpiFx_FOUND)
       message(STATUS "Found installed MpiFx package")
