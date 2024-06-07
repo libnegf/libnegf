64a8cb120d5ef4d7ebfcebec1d710f41c3d41bff
Christoph Conrads <c.conrads@fz-juelich.de>
2024-06-07 15:59:44 +0200

    Tests: remove unused variables in a C test

--- tests/c_int_file/hello.c
+++ tests/c_int_file/hello.c
@@ -6,27 +6,17 @@
 
 int main()
 {
-
   int handler[NEGF_HSIZE];
-  int *hand = &handler[0];
-  char realmat[7] = "HR.dat";
-  char imagmat[7] = "HI.dat";
-  struct lnparams params;
-  int surfstart[2] = {61,81};
-  int surfend[2] = {60,80};
-  int contend[2] = {80,100};
-  int plend[1] = {60};
-  int cblk[2] = {1,1};
   
   printf("Initializing libNEGF \n");
-  negf_init_session(hand);
-  negf_init(hand);
-  negf_init_contacts(hand, 2);
-  negf_read_input(hand);
-  negf_solve_landauer(hand);
-  negf_write_tunneling_and_dos(hand);
-  negf_destruct_libnegf(hand);
-  negf_destruct_session(hand);
+  negf_init_session(handler);
+  negf_init(handler);
+  negf_init_contacts(handler, 2);
+  negf_read_input(handler);
+  negf_solve_landauer(handler);
+  negf_write_tunneling_and_dos(handler);
+  negf_destruct_libnegf(handler);
+  negf_destruct_session(handler);
   printf("Done \n");
 
 }
